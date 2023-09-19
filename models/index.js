875d2b2af2006258c879daee75e2d2b708a41bdd
Youssef <youssefkaroui6@gmail.comm>
2023-09-19 17:48:27 -0400

    models index.js done, includes all the relationships required by the assignment

--- models/index.js
+++ models/index.js
@@ -11,11 +11,13 @@ Product.belongsTo(Category, {
   onDelete: 'CASCADE'
 });
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id'});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag, foreignKey: 'tag_id'});
 
 module.exports = {
   Product,
